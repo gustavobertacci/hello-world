621ae5024f322dd6e87eafb8a421f61da8a10d47
gustavobertacci <gustavobertacci@gmail.com>
2023-10-10 10:25:02 -0300

    oiiii

---
+++ src/app/post/page.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+
+export default function Post3 () {
+    return (
+        <div>
+            <hi>Postagens</hi>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, unde officia? Odio facilis maxime amet illo eligendi culpa quos. Nesciunt fugiat sunt doloribus. Cum unde recusandae, tenetur porro ipsa voluptate?</p>
+            <p> <Link href="/">Voltar...</Link></p>
+
+        </div>
+    )
+
+}
---
+++ src/app/post/post1/page.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Post1 () {
+    return (
+        <div>
+            <hi>Postagens</hi>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, unde officia? Odio facilis maxime amet illo eligendi culpa quos. Nesciunt fugiat sunt doloribus. Cum unde recusandae, tenetur porro ipsa voluptate?</p>
+            <p> <Link href="/">Voltar...</Link></p>
+        </div>
+    )
+
+}
